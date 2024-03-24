664f9e3e36225c1dd18786cb4eaf86719c35ed8e
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-03-24 17:26:28 +0200

    Added login decorator and updated accounts model

--- ecommerce_store/accounts/views.py
+++ ecommerce_store/accounts/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 def user_login(request):
@@ -33,3 +32,17 @@ def user_register(request):
 def user_profile(request):
     profile = Profile.objects.get(user=request.user)
     return render(request, 'accounts/profile.html', {'profile': profile})
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log the user out and log them back in to ensure password change is effective
+            logout(request)
+            login(request, user)
+            return redirect('accounts:profile')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {'form': form})
--- ecommerce_store/ecommerce_store/urls.py
+++ ecommerce_store/ecommerce_store/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +9,8 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('cart.urls')),
     path('orders/', include('orders.urls')),
+    path('search/', include('search.urls')),
+    path('pages/', include('pages.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
