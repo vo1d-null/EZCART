88a286767b01bb254dfb96bef750532b8f55ee80
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-03-24 17:14:17 +0200

    Cart logic implementation and path config

--- ecommerce_store/cart/views.py
+++ ecommerce_store/cart/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from store.models import Product
+from .models import Cart, CartItem
 
-# Create your views here.
+def cart_detail(request):
+    cart = Cart.objects.get(user=request.user)
+    cart_items = CartItem.objects.filter(cart=cart)
+    return render(request, 'cart/cart_detail.html', {'cart_items': cart_items})
+
+def cart_add(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('cart:cart_detail')
+
+def cart_remove(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = Cart.objects.get(user=request.user)
+    cart_item = CartItem.objects.get(cart=cart, product=product)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart:cart_detail')
