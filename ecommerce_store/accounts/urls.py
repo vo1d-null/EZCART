0859ba9219bd584a675fc2a51c4db7859cf71ab3
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-03-24 16:58:57 +0200

    Configured url paths

---
+++ ecommerce_store/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
+    path('profile/', views.user_profile, name='profile'),
+]
