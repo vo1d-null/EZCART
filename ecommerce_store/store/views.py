d4839caedef67c6d508c710b903296bec232efee
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-03-25 15:33:09 +0200

    Added custom tags and restructured the view logic

--- ecommerce_store/store/views.py
+++ ecommerce_store/store/views.py
@@ -1,19 +1,47 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
+from django.db.models import Count, Q
 from .models import Product, Category
 
-def product_list(request):
-    products = Product.objects.all()
-    categories = Category.objects.all()
+class ProductListView(ListView):
+    model = Product
+    template_name = 'store/product_list.html'
+    paginate_by = 12
+    ordering = ['-id']  # Order by newest products first
 
-    # Filter products by category
-    category_id = request.GET.get('category')
-    if category_id:
-        products = products.filter(category__id=category_id)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.request.GET.get('category')
+        query = self.request.GET.get('q')
+        price_range = self.request.GET.get('price_range')
 
-    # Paginate the product list
-    paginator = Paginator(products, 12)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+        if category_id:
+            queryset = queryset.filter(category__id=category_id)
 
-    return render(request, 'store/product_list.html', {'page_obj': page_obj, 'categories': categories})
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+
+        if price_range:
+            min_price, max_price = price_range.split('-')
+            queryset = queryset.filter(price__gte=min_price, price__lte=max_price)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.annotate(product_count=Count('product'))
+        price_ranges = [
+            ('0-100', 'Under $100'),
+            ('100-500', '$100 - $500'),
+            ('500-1000', '$500 - $1000'),
+            ('1000-', 'Over $1000'),
+        ]
+        context['categories'] = categories
+        context['price_ranges'] = price_ranges
+        return context
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'store/product_detail.html'
