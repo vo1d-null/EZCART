a143f296ca862543136edc1bf46d780c54a54836
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-03-29 13:20:39 +0200

    Reorganizing final

--- EZCart_Project/demo/settings.py
+++ EZCart_Project/demo/settings.py
@@ -116,3 +116,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STRIPE_PUBLIC_KEY = 'pk_test_lX3r6OMjOU2yzFsNSHq6belT00EY82kZmH'
 STRIPE_SECRET_KEY = 'sk_test_tn0CTDaIJHUJyAqhsf39cfsC00LNjsqDnb'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
