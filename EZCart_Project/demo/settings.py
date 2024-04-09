2a173dfa5088f91f3398ce4f21a2aa9434bef2c7
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-04-09 21:47:32 +0300

    prod2

--- EZCart_Project/demo/settings.py
+++ EZCart_Project/demo/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'crispy_forms',
     'django_countries',
-    'core'
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -67,9 +67,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('RAILWAY_TCP_PROXY_DOMAIN'),
+        'PORT': os.getenv('RAILWAY_TCP_PROXY_PORT'),
     }
 }
 
