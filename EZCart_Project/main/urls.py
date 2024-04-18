9056874052d7c698eff57bc74017925ce150ce97
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-04-18 23:44:19 +0300

    +

--- EZCart_Project/main/urls.py
+++ EZCart_Project/main/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path 
+from django.urls import path
+from django.conf.urls.static import static 
+from EZCart_Project.ecommerce import settings 
 from . import views 
 
 
@@ -6,3 +8,9 @@ urlpatterns = [
     path('', views.main_page, name='home'),
     path('about/', views.about, name='about')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
