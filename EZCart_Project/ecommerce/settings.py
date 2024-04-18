af55d3daa8a3fc8411038fd8d743461616f6d7aa
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-04-18 14:30:49 +0300

    -

--- EZCart_Project/ecommerce/settings.py
+++ EZCart_Project/ecommerce/settings.py
@@ -151,6 +151,15 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_REDIRECT_EXEMPT = []
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
