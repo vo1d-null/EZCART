e2fb21c5082113cd60345d9dffa85878b0300c7a
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-04-18 21:28:18 +0300

    +

--- EZCart_Project/ecommerce/settings.py
+++ EZCart_Project/ecommerce/settings.py
@@ -119,15 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where 'collectstatic' will gather your static files
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+ 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     ]
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
