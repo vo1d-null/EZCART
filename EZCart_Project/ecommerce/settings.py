4b988361af5e1dee3a81efac0403d0cdfb90fd1c
Nikolay Gerginov <123015737+vo1d-null@users.noreply.github.com>
2024-04-18 20:47:14 +0300

    +

--- EZCart_Project/ecommerce/settings.py
+++ EZCart_Project/ecommerce/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where 'collectstatic' will gather your static files
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     ]
